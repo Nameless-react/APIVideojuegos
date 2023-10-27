da02e263a3fe5db145902933c4aa42bd75310e54
Joel <joel33960gmail.com>
2023-10-27 11:36:49 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+
+
+const app = express();
+const port = 3000;
+
+
+
+app.listen(port, () => {
+    console.log(`El servidor está corriendo en el puerto ${port}`);   
+})
